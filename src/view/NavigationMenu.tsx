4c4ec6dd4532564ee3f30984b22bf3e33d68c29a
Patrick Shaw <psha67@student.monash.edu>
2018-04-06 04:59:12 +1000

    Fixed NavigationMenu actionName
    Reversed the order

--- src/view/NavigationMenu.tsx
+++ src/view/NavigationMenu.tsx
@@ -1,15 +1,16 @@
-import React, { ComponentType } from 'react';
+import React, { SFC } from 'react';
 import Drawer from 'material-ui/Drawer';
 import * as Icons from 'material-ui-icons';
-import { withStyles, List, ListItem, ListItemIcon, ListItemText } from 'material-ui';
+import { List, ListItem, ListItemIcon, ListItemText } from 'material-ui';
 import { state as profileState } from 'state/ProfileState';
 import { Route } from 'react-router-dom';
 import { observable, action, autorun, computed } from 'mobx';
 import { observer, Observer } from 'mobx-react';
+import { createStyled } from 'view/createStyled';
 import Divider from 'material-ui/Divider';
 import classNames from 'classnames';
 // TODO: Maybe come back to this and see if we can get proper type validation going
-const styles: any = theme => ({
+const Styled: any = createStyled(theme => ({
   navPaper: {
     overflowX: 'hidden',
     position: 'relative'
@@ -31,14 +32,14 @@ const styles: any = theme => ({
       duration: theme.transitions.duration.enteringScreen
     })
   }
-});
+}));
 export interface NavigationMenuProps {}
 // TODO: In this case, it would probably be better if we didn't use the same state for all NavigationMenu components
 class NavigationState {
   @observable public open: boolean = false;
   @computed
   public get actionName(): string {
-    return this.open ? 'Open' : 'Close';
+    return this.open ? 'Closed' : 'Open';
   }
   @action
   public toggleOpen(): void {
@@ -52,51 +53,53 @@ const NavigationButton = ({ icon, primary, ...other }) => (
     <ListItemText primary={primary} />
   </ListItem>
 );
-export const NavigationMenu: ComponentType<NavigationMenuProps> = withStyles(styles, {
-  withTheme: true
-})(({ classes }) => (
-  <Route
-    render={({ history }) => (
-      /* TODO: Unfortunately, render callbacks aren't considered with MobX's observer so we have to use the <Observer> syntax. 
-        There might be a better way. */
-      <Observer>
-        {() => (
-          <Drawer
-            variant="permanent"
-            classes={{
-              paper: classNames(
-                classes.navPaper,
-                navState.open ? classes.navPaperOpen : classes.navPaperClosed
-              )
-            }}
-            open={navState.open}
-          >
-            <List component="nav">
-              <NavigationButton
-                onClick={() => navState.toggleOpen()}
-                icon={<Icons.ChevronRight />}
-                primary={navState.actionName}
-              />
-              <Divider />
-              <NavigationButton
-                onClick={() => history.push('/')}
-                icon={<Icons.Dashboard />}
-                primary="Overview"
-              />
-              <NavigationButton
-                onClick={() => history.push(`/profiles/${profileState.me.id}`)}
-                icon={<Icons.AccountCircle />}
-                primary="Account"
-              />
-              <NavigationButton
-                onClick={() => history.push('/settings')}
-                icon={<Icons.Settings />}
-                primary="Settings"
-              />
-            </List>
-          </Drawer>
+export const NavigationMenu: SFC<NavigationMenuProps> = () => (
+  <Styled>
+    {({ classes }) => (
+      <Route
+        render={({ history }) => (
+          /* TODO: Unfortunately, render callbacks aren't considered with MobX's observer so we have to use the <Observer> syntax. 
+            There might be a better way. */
+          <Observer>
+            {() => (
+              <Drawer
+                variant="permanent"
+                classes={{
+                  paper: classNames(
+                    classes.navPaper,
+                    navState.open ? classes.navPaperOpen : classes.navPaperClosed
+                  )
+                }}
+                open={navState.open}
+              >
+                <List component="nav">
+                  <NavigationButton
+                    onClick={() => navState.toggleOpen()}
+                    icon={navState.open ? <Icons.ChevronLeft /> : <Icons.ChevronRight />}
+                    primary={navState.actionName}
+                  />
+                  <Divider />
+                  <NavigationButton
+                    onClick={() => history.push('/')}
+                    icon={<Icons.Dashboard />}
+                    primary="Overview"
+                  />
+                  <NavigationButton
+                    onClick={() => history.push(`/profiles/${profileState.me.id}`)}
+                    icon={<Icons.AccountCircle />}
+                    primary="Account"
+                  />
+                  <NavigationButton
+                    onClick={() => history.push('/settings')}
+                    icon={<Icons.Settings />}
+                    primary="Settings"
+                  />
+                </List>
+              </Drawer>
+            )}
+          </Observer>
         )}
-      </Observer>
+      />
     )}
-  />
-));
+  </Styled>
+);
