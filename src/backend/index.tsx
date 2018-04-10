19c928eb6b24e1039ca3dc0f2e52e54532b75309
Tim Gebert <kingroguemc@gmail.com>
2018-04-10 22:08:55 +1000

    Added Api method to generate an sdk, and empty function to handle it

--- src/backend/index.tsx
+++ src/backend/index.tsx
@@ -1,12 +1,36 @@
 import express from 'express';
+import { generateSdk } from 'client/sdkGeneration';
+import bodyParser from 'body-parser';
+import { Specification } from 'model/Specification';
+import { getSpecificationById, getSpecifications } from 'backend/specifications';
+
 async function run(port: number) {
   const app: express.Express = express();
+  app.use(bodyParser.json());
   app.get('/', (req, res) => {
     res.json({
       status: 'Success',
       message: 'It works!'
     });
   });
+
+  /* API method to generate an sdk for a given specification
+  * @params {string} req.body.id - ID of the specification to generate the SDK for
+  * @return {Promise<string>} - The URL that the SDK can be downloaded from
+  */
+  app.post('/generatesdk', async (req, res) => {
+    const specificationId: number = req.body.id;
+    var spec: Specification | undefined = getSpecificationById(specificationId);
+
+    if (spec != undefined) {
+      const sdkUrl: String = await generateSdk(spec);
+      res.send(sdkUrl);
+    } else {
+      res.statusCode = 404;
+      res.send(null);
+    }
+  });
+
   app.listen(port);
 }
 const envPort: string | undefined = process.env.PORT;
---
+++ src/client/sdkGeneration.tsx
@@ -0,0 +1,14 @@
+import { Specification } from 'model/Specification';
+
+export async function generateSdk(spec: Specification): Promise<string> {
+  /* Generates an SDK for a specification 
+  * @param {Specification} spec - The specification object for which the sdk needs to be generated
+  * @return {Promise<string>} - The URL from which the sdk can be downloaded
+  */
+
+  //TODO
+
+  var sdkUrl: string = 'exampleSdkUrl.io';
+
+  return sdkUrl;
+}
