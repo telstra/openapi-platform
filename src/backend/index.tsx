e1cda7898641d7e1a7d48042f346e325f9f5a936
Tim Gebert <tgeb3@student.monash.edu>
2018-04-13 10:34:16 +1000

    Changed api response types. Moved function comments.

--- src/backend/index.tsx
+++ src/backend/index.tsx
@@ -18,25 +18,29 @@ async function run(port: number) {
     });
   });
 
-  /* API method to generate an sdk for a given specification
-  * @params {string} req.body.id - ID of the specification to generate the SDK for
-  * @return {Promise<string>} - The URL that the SDK can be downloaded from
-  */
+  /** API method to generate an sdk for a given specification
+   * @param {string} req.body.id - ID of the specification to generate the SDK for
+   * @return {Promise<string>} - The URL that the SDK can be downloaded from
+   */
   app.post('/generatesdk', async (req, res) => {
     const specificationId: number = req.body.id;
     var spec: Specification | undefined = getSpecificationById(specificationId);
 
     if (spec != undefined) {
       const sdkUrl: String = await generateSdk(spec);
-      res.send(sdkUrl);
+      res.json({
+        status: 'success',
+        url: sdkUrl
+      });
     } else {
-      res.statusCode = 404;
-      res.send(null);
+      res.json({
+        status: 'failure'
+      });
     }
   });
 
-  /* API Method to add a specification
-   * @params {string} req.body.title - optional parameter to specify title of Specification
+  /** API Method to add a specification
+   * @param {string} req.body.title - optional parameter to specify title of Specification
    * @return {Promise<Specification>} - The Specification that was created
    */
   app.post('/addspecification', async (req, res) => {
@@ -47,7 +51,7 @@ async function run(port: number) {
     } else {
       spec = addSpecification();
     }
-    res.send(spec);
+    res.json(spec);
   });
 
   app.listen(port);
--- src/backend/specifications.tsx
+++ src/backend/specifications.tsx
@@ -2,18 +2,18 @@ import { Specification } from 'model/Specification';
 
 // TODO: Replace this list
 let count = 0;
-let specifications: Specification[] = [
+const specifications: Specification[] = [
   {
     id: 1234,
     title: 'TestSpecification'
   }
 ];
 
-export function getSpecificationById(id: number): Specification | undefined {
-  /* Gets a specification with a matching id to what is provided
- * @params {number} id - the id of the specification to fetch
+/** Gets a specification with a matching id to what is provided
+ * @param {number} id - the id of the specification to fetch
  * @return {Specification | undefined} - returns the Specification with the matching id if it exists
  */
+export function getSpecificationById(id: number): Specification | undefined {
   for (const spec of specifications) {
     if (spec.id === id) {
       return spec;
@@ -22,18 +22,18 @@ export function getSpecificationById(id: number): Specification | undefined {
   return undefined;
 }
 
-export function getSpecifications(): Specification[] {
-  /* Gets all Specifications 
+/** Gets all Specifications
  * @return {Specification[]} - returns an array containing all specifications
  */
+export function getSpecifications(): Specification[] {
   return specifications;
 }
 
+/** Adds a specification to the list
+ * @param {string} titleString - optional argument to provide a specification title
+ * @return {Specification} - specification object that was created
+ */
 export function addSpecification(titleString?: string): Specification {
-  /* Adds a specification to the list 
-   * @param {string} titleString - optional argument to provide a specification title
-   * @return {Specification} - specification object that was created
-   */
   console.log('adding specification with id: ' + count);
   let spec: Specification;
   if (titleString) {
