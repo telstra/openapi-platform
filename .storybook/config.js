1b87bb9b1f5eebb42bea735448c5bfe6841c94b0
Patrick Shaw <psha67@student.monash.edu>
2018-05-04 04:36:14 +1000

    Added basic Storybook setup

---
+++ .storybook/config.js
@@ -0,0 +1,22 @@
+import { configure } from '@storybook/react';
+import { createStories } from './createStories';
+import { withKnobs } from '@storybook/addon-knobs';
+import { checkA11y } from '@storybook/addon-a11y';
+import { withViewport } from '@storybook/addon-viewport';
+import { setOptions } from '@storybook/addon-options';
+import backgrounds from '@storybook/addon-backgrounds';
+import { addDecorator } from '@storybook/react';
+addDecorator(withKnobs);
+addDecorator(checkA11y);
+addDecorator(withViewport('responsive'));
+addDecorator(backgrounds());
+setOptions({
+  name: 'Swagger Platform'
+});
+configure(() => {
+  const tsxModuleContext = require.context('view', true, /\.tsx$/);
+  const moduleInfo = tsxModuleContext
+    .keys()
+    .map(path => ({ path, requiredModule: tsxModuleContext(path) }));
+  createStories(moduleInfo);
+}, module);
