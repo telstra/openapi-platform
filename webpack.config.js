cb0ac515922682809064c056b6dce135dee81e4d
darvid7 <david.anthony.lei@gmail.com>
2018-07-12 18:43:51 +1000

    Remove un-needed babel plugin (i think)

--- webpack.config.js
+++ webpack.config.js
@@ -69,7 +69,6 @@ module.exports = (env, argv) => {
                   regenerator: true,
                 },
               ],
-              ['@babel/plugin-proposal-export-default-from'],
             ],
           },
         },
