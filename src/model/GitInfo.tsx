da26c541a5848f399367f6d4af1d026309af197f
Patrick Shaw <psha67@student.monash.edu>
2018-08-16 23:06:44 +1000

    Added better Git authentication
    ty Caleb

--- src/model/GitInfo.tsx
+++ src/model/GitInfo.tsx
@@ -1,14 +1,37 @@
 /**
- * Note: At the moment this is pretty much an exact replica of the auth fields from
- * isomorphic-git https://isomorphic-git.github.io/docs/en/push
+ * Follows Basic authentication - Just a username and password
  */
-
-export interface GitAuth {
+interface GitAuthBasic {
   username: string;
-  token?: string;
-  password?: string;
-  oauth2format?: 'github' | 'bitbucket' | 'gitlab';
+  password: string;
+  token?: never;
+  oauth2format?: never;
 }
+
+/**
+ * Follows personal access token authentication
+ * @see https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/
+ * @see https://confluence.atlassian.com/bitbucket/app-passwords-828781300.html
+ * @see https://docs.gitlab.com/ee/user/profile/personal_access_tokens.html
+ */
+interface GitAuthToken {
+  username: string;
+  password?: never;
+  token: string;
+  oauth2format?: never;
+}
+
+/**
+ * For OAuth2 authentication
+ */
+interface GitAuthOAuth2 {
+  username?: never;
+  password?: never;
+  token: string;
+  oauth2format: 'github' | 'bitbucket' | 'gitlab';
+}
+
+export type GitAuth = GitAuthBasic | GitAuthToken | GitAuthOAuth2;
 
 export interface GitInfo {
   repoUrl: string;
