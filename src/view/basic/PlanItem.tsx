d2db3b88352b0502137438fc794bf7fdfc3b736a
Caleb Joseph <calebj0seph@users.noreply.github.com>
2018-08-16 23:50:01 +1000

    Tidy up PlanItem by moving all the style overrides into the TableRow instead of the TableCell

--- src/view/basic/PlanItem.tsx
+++ src/view/basic/PlanItem.tsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 
 import { Button, Typography, IconButton, TableRow, TableCell } from '@material-ui/core';
 import * as Icons from '@material-ui/icons';
-import classNames from 'classnames';
 import { observable, action } from 'mobx';
 import { Observer } from 'mobx-react';
 
@@ -14,14 +13,17 @@ import { Sdk } from 'model/Sdk';
 import { createStyled } from 'view/createStyled';
 
 const Styled: any = createStyled(theme => ({
-  planItemCell: {
-    border: 'none',
-    padding: [0, theme.spacing.unit / 2],
-    '&:last-child': {
-      paddingRight: theme.spacing.unit * 3,
-    },
-    '&:first-child': {
-      paddingLeft: theme.spacing.unit * 3,
+  planItemRow: {
+    color: 'red',
+    '& > td': {
+      border: 'none',
+      padding: [0, theme.spacing.unit / 2],
+      '&:last-child': {
+        paddingRight: theme.spacing.unit * 3,
+      },
+      '&:first-child': {
+        paddingLeft: theme.spacing.unit * 3,
+      },
     },
   },
   planStatus: {
@@ -63,11 +65,11 @@ export class PlanItem extends Component<PlanItemProps> {
         {({ classes }) => (
           <Observer>
             {() => (
-              <TableRow>
-                <TableCell padding="none" classes={{ root: classes.planItemCell }}>
+              <TableRow classes={{ root: classes.planItemRow }}>
+                <TableCell>
                   <Typography className={classes.planTitle}>{plan.target}</Typography>
                 </TableCell>
-                <TableCell padding="none" classes={{ root: classes.planItemCell }}>
+                <TableCell>
                   <Typography
                     className={classes.planVersion}
                     variant="body1"
@@ -76,19 +78,12 @@ export class PlanItem extends Component<PlanItemProps> {
                     {plan.version}
                   </Typography>
                 </TableCell>
-                <TableCell
-                  padding="none"
-                  classes={{ root: classNames(classes.planStatus, classes.planItemCell) }}
-                >
+                <TableCell classes={{ root: classes.planStatus }}>
                   <div className={classes.planStatus}>
                     <BuildStatusChip buildStatus={plan.buildStatus} />
                   </div>
                 </TableCell>
-                <TableCell
-                  numeric
-                  padding="none"
-                  classes={{ root: classes.planItemCell }}
-                >
+                <TableCell numeric>
                   <div className={classes.planActions}>
                     {this.latestSdkUrl ? (
                       <IconButton href={this.latestSdkUrl}>
