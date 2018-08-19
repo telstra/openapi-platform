7457dcbc88266cdc711f89119ac326e059785881
Patrick Shaw <psha67@student.monash.edu>
2018-08-19 17:01:13 +1000

    Woops :P

--- openapi-platform.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  database: {
-    name: 'swagger_platform',
-    host: '***REMOVED***',
-    port: 5432,
-    username: 'swagger_platform',
-    password: '***REMOVED***',
-  },
-};
