0eddab379360aae862149938e0d620a0470e2091
Patrick Shaw <psha67@student.monash.edu>
2018-08-20 00:17:54 +1000

    Renamed View to view in imports

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -6,8 +6,8 @@ import { BrowserRouter } from 'react-router-dom';
 import 'typeface-roboto';
 import 'typeface-roboto-mono';
 
-import { Page } from './View/Page';
-import { ThemeProvider } from './View/ThemeProvider';
+import { Page } from './view/Page';
+import { ThemeProvider } from './view/ThemeProvider';
 
 jss
   .use(jssGlobal.default())
