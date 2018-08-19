3d657c5c1a91139764d24e6a643c07b927ce85a6
Patrick Shaw <psha67@student.monash.edu>
2018-08-19 22:38:45 +1000

    Added file compatibility tests for config parsing

--- packages/server/src/config/index.tsx
+++ packages/server/src/config/index.tsx
@@ -1,30 +1,3 @@
-import { Config } from 'convict';
-import { pathExistsSync } from 'fs-extra';
-import { join } from 'path';
-import { schema } from './schema';
-
-export function parse<T>(): Config<T> {
-  const cwd = process.cwd();
-  let parsedConfig: Config<T> | undefined;
-  const extensions = ['json', 'yaml', 'yml', 'json5'];
-  for (const ext of extensions) {
-    try {
-      const path = join(cwd, `openapi-platform.config.${ext}`);
-      if (pathExistsSync(path)) {
-        parsedConfig = schema.loadFile(path);
-        break;
-      }
-    } catch (err) {
-      parsedConfig = undefined;
-    }
-  }
-  if (!parsedConfig) {
-    throw new Error(
-      `Failed to find an openapi-platform.config.${extensions.join('/')} file`,
-    );
-  }
-  schema.validate({ allowed: 'strict' });
-  return parsedConfig;
-}
+import { readConfig } from './readConfig';
 // TODO: We can provide more ways to specify configs in the future
-export const config = parse();
+export const config = readConfig();
---
+++ packages/server/src/config/readConfig.tsx
@@ -0,0 +1,28 @@
+import { Config } from 'convict';
+import { pathExistsSync } from 'fs-extra';
+import { join } from 'path';
+import { schema } from './schema';
+
+export function readConfig<T>(): Config<T> {
+  const cwd = process.cwd();
+  let parsedConfig: Config<T> | undefined;
+  const extensions = ['json', 'yaml', 'yml', 'json5'];
+  for (const ext of extensions) {
+    try {
+      const path = join(cwd, `openapi-platform.config.${ext}`);
+      if (pathExistsSync(path)) {
+        parsedConfig = schema.loadFile(path);
+        break;
+      }
+    } catch (err) {
+      parsedConfig = undefined;
+    }
+  }
+  if (!parsedConfig) {
+    throw new Error(
+      `Failed to find an openapi-platform.config.${extensions.join('/')} file`,
+    );
+  }
+  schema.validate({ allowed: 'strict' });
+  return parsedConfig;
+}
