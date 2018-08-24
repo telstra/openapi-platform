b0fd896f0fc3fe6ffe0091f6088ace442c829c27
Patrick Shaw <psha67@student.monash.edu>
2018-08-24 11:34:52 +1000

    server bin file now actually runs the server

--- packages/server/bin/openapi-platform-server.js
+++ packages/server/bin/openapi-platform-server.js
@@ -1,6 +1,2 @@
-const { schema } = require('@openapi-platform/config');
-schema.load({});
-console.log(schema.get('server.port'));
-
-/*const { run } = require('../lib/index');
-run();*/
+const { run } = require('../lib/index');
+run();
