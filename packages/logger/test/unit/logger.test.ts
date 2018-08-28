49ca3f3cf505afec1f3ed94973c99139eeb99db3
Tim Gebert <tgeb3@student.monash.edu>
2018-08-28 16:04:04 +1000

    Added tests to check that the logger is outputting the correct text

--- packages/logger/test/unit/logger.test.ts
+++ packages/logger/test/unit/logger.test.ts
@@ -1,6 +1,9 @@
 import { mockFunctions } from 'jest-mock-functions';
-import { openapiLogger, overrideConsoleLogger } from '../../src';
+import { openapiLogger, overrideConsoleLogger , consoleTransport} from '../../src';
+import MockDate from 'mockdate';
+
 const mockLogger: any = mockFunctions(openapiLogger(), { recursive: true });
+const loggingMethods = ['error', 'debug', 'info'];
 
 /**
  * Checks which logging methods in the custom logger were actually called
@@ -25,7 +28,6 @@ function testConsoleInput(...input) {
       console.log(...input);
       testLoggerCallCounts({ verbose: 1 });
     });
-    const loggingMethods = ['error', 'debug', 'info'];
     loggingMethods.forEach(loggingMethodName => {
       it(`logging to console.${loggingMethodName}}`, () => {
         overrideConsoleLogger(mockLogger);
@@ -49,3 +51,95 @@ describe('console logs to custom logger', () => {
   testConsoleInput(/reaewr/g);
   testConsoleInput(new Error('message goes here'));
 });
+
+// Set timezone to const value then adjust for timezone
+MockDate.set('2018-05-03T12:34:56z');
+const tempdate = new Date();
+const adjustedTime = tempdate.getTime() + tempdate.getTimezoneOffset() * 60 * 1000;
+MockDate.set(adjustedTime);
+
+// replace stdout.write so we can capture output
+let lastMessage = '';
+const old_write = process.stdout.write;
+process.stdout.write = (function(write) {
+  return function(string, encoding, fd) {
+    write.apply(process.stdout, arguments);
+    lastMessage = string.strip.trim();
+  };
+})(process.stdout.write);
+
+const loggerNames = ['debug', 'info', 'warn'];
+const loggers = [
+  openapiLogger().add(consoleTransport({ level: 'debug' })).debug,
+  openapiLogger().add(consoleTransport({ level: 'info' })).info,
+  openapiLogger().add(consoleTransport({ level: 'warn' })).warn,
+]
+
+for (let i = 0; i < loggerNames.length; i++) {
+  const name = loggerNames[i];
+  const testLogger = loggers[i];
+
+  function formatExpected(expectedString) {
+    const prefix: String = '2018-05-03 12:34:56 ';
+    const padding: String = ' '.repeat(9 - name.length);
+    const output: String = (prefix + name + padding + expectedString).strip.trim();
+    return output;
+  }
+
+  describe(name + ' logger tests', () => {
+    it('Simple String', () => {
+      testLogger('a message :)');
+      expect(lastMessage).toBe(formatExpected('a message :)'));
+    });
+
+    it('Undefined', () => {
+      testLogger(undefined);
+      expect(lastMessage).toBe(formatExpected('undefined'));
+    });
+
+    it('Empty String', () => {
+      testLogger('');
+      expect(lastMessage).toBe(formatExpected(''));
+    });
+
+    it('Single character', () => {
+      testLogger('a');
+      expect(lastMessage).toBe(formatExpected('a'));
+    });
+
+    it('List of strings', () => {
+      testLogger(['test', 'test2', 'test3']);
+      expect(lastMessage).toBe(formatExpected("[ 'test', 'test2', 'test3' ]"));
+    });
+
+    it('Multiple Parameters', () => {
+      testLogger('this', 'is', 'a', 'test');
+      expect(lastMessage).toBe(formatExpected('this is a test'));
+    });
+
+    it('JSON', () => {
+      testLogger({ also: 'a', test: ':D' });
+      expect(lastMessage).toBe(formatExpected("{ also: 'a', test: ':D' }"));
+    });
+
+    it('Number', () => {
+      testLogger(0);
+      expect(lastMessage).toBe(formatExpected('0'));
+    });
+
+    it('Function', () => {
+      testLogger(() => {});
+      expect(lastMessage).toBe(formatExpected('[Function]'));
+    });
+
+    it('Regex', () => {
+      testLogger(/reaewr/g);
+      expect(lastMessage).toBe(formatExpected('/reaewr/g'));
+    });
+
+    it('Error', () => {
+      testLogger(new Error('Test error logging'));
+      expect(lastMessage).toContain(formatExpected('Error: Test error logging'));
+    });
+  });
+}
