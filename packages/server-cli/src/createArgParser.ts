f82891da6637ef9feeb2dbd75dbf26f5dddf948c
Tim Gebert <tgeb3@student.monash.edu>
2018-10-24 10:22:15 +1100

    Updated list function arg parsing code

--- packages/server-cli/src/createArgParser.ts
+++ packages/server-cli/src/createArgParser.ts
@@ -1,17 +1,27 @@
 import { Command } from 'commander';
 
+import { listItems } from './list';
 import { remove } from './remove';
 
 export function createArgParser(): Command {
   const root = new Command();
-  root.version('1.0.0-alpha.0').command('add <type>', 'Add an item');
+  root
+    .version('1.0.0-alpha.0')
+    .command('add <type>')
+    .description('Add an item to the db');
 
-  root.command('list <type> [filters...]', 'List items in db');
+  root
+    .command('list <type> [filters...]')
+    .description('List items in db')
+    .action(listItems);
 
   root
     .command('remove <type> [ids...]')
     .description('Remove items from db')
-    .action(remove)
+    .action(remove);
+  
+    root
     .command('build <spec id> <sdkConfigIds...>', 'Runs the build process for an SDK');
+
   return root;
 }
--- packages/server-cli/src/list.ts
+++ packages/server-cli/src/list.ts
@@ -1,5 +1,3 @@
-import program from 'commander';
-
 import { client, socket } from './client';
 import { config } from './config';
 import { logger } from './logger';
@@ -25,7 +23,7 @@ export async function getItems(type: string) {
   return itemIds;
 }
 
-async function listItems(type, filters) {
+export async function listItems(type, filters) {
   if (['specifications', 'sdkConfigs', 'sdks'].indexOf(type) >= 0) {
     const items = await getItems(type);
     for (const item of items) {
@@ -37,10 +35,3 @@ async function listItems(type, filters) {
     );
   }
 }
-
-program.parse(process.argv);
-
-const itemType = program.args[0];
-const itemFilters = program.args.slice(1, program.args.length);
-
-listItems(itemType, itemFilters);
